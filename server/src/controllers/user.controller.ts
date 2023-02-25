b1de27eb6331b01d15333b8bef4480021bc01f28
Nicholas Otieno <niklasoti@gmail.com>
2023-02-25 22:29:22 +0100

    Update

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -3,72 +3,88 @@ import User from "../models/user.model";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import validator from "validator";
+import asyncHandler from "express-async-handler";
 
 const createToken = (id: string) => {
   const { JWT_SECRET = "" } = process.env;
   return jwt.sign({ id }, JWT_SECRET, { expiresIn: "3d" });
 };
 
-const signupUser = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-  try {
-    if (!email || !password) {
-      throw Error("All fields must be field");
-    }
-
-    if (!validator.isEmail(email)) {
-      throw Error("Email is not valid");
-    }
-
-    if (!validator.isStrongPassword(password)) {
-      throw Error("Password not strong enough");
-    }
-
-    const exists = await User.findOne({ email });
-    if (exists) {
-      throw Error("Email already in use");
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-    const user = await User.create({ email, password: hash });
-
-    // Create token
-    const token = createToken(user.id);
-
-    res.status(200).json({ email, token });
-  } catch (error: any) {
-    res.status(400).json({ error: error.message });
-  }
-};
-
-const loginUser = async (req: Request, res: Response) => {
+// @desc Login User
+// @route POST  /api/users/login
+// @access Public
+const signupUser = asyncHandler(async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
-  try {
-    if (!email || !password) {
-      throw Error("All fields must be field");
-    }
-
-    const user = await User.findOne({ email });
-
-    if (!user) {
-      throw Error("Incorect Email");
-    }
-
-    const match = await bcrypt.compare(password, user.password as string);
-
-    if (!match) {
-      throw Error("Incorrect Password");
-    }
-
-    // Create token
-    const token = createToken(user.id);
-
-    res.status(200).json({ email, token });
-  } catch (error: any) {
-    res.status(400).json({ error: error.message });
+  if (!email || !password) {
+    res.status(400);
+    throw Error("All fields must be field");
   }
-};
 
-export { loginUser, signupUser };
+  if (!validator.isEmail(email)) {
+    res.status(400);
+    throw Error("Email is not valid");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    res.status(400);
+    throw Error("Password not strong enough");
+  }
+
+  const exists = await User.findOne({ email });
+
+  if (exists) {
+    res.status(400);
+    throw Error("Email already in use");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  const user = await User.create({ email, password: hash });
+
+  // Create token
+  const token = createToken(user.id);
+
+  res.status(200).json({ email, token });
+});
+
+// @desc Login User
+// @route POST  /api/users/login
+// @access Public
+const loginUser = asyncHandler(async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400);
+    throw Error("All fields must be field");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    res.status(401);
+    throw Error("Incorect Email");
+  }
+
+  const match = await bcrypt.compare(password, user.password as string);
+
+  if (!match) {
+    res.status(401);
+    throw Error("Incorrect Password");
+  }
+
+  // Create token
+  const token = createToken(user.id);
+
+  res.status(200).json({ email, token });
+});
+
+// @desc Get User Data
+// @route GET  /api/users/me
+// @access Private
+const getMe = asyncHandler(async (req: any, res) => {
+  const { id, email } = (await User.findById(req.user.id)) as any;
+  res.status(200).json({ id, email });
+});
+
+export { loginUser, signupUser, getMe };
--- server/src/routes/user.router.ts
+++ server/src/routes/user.router.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { loginUser, signupUser } from "../controllers/user.controller";
+import { loginUser, signupUser, getMe } from "../controllers/user.controller";
+import { requireAuth } from "../middleware/auth.middleware";
 
 const router = express.Router();
 
 router.post("/login", loginUser);
 router.post("/signup", signupUser);
+router.get("/me", requireAuth, getMe);
 
 export default router;
